3cfbdabaabac3953c23811da5fbb8f3a00883b41
beillrebecca <noanh0810@gmail.com>
2026-04-03 09:59:03 +0900

    script.js

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if(signupBtn){
     signupBtn.addEventListener("click", () => {
-      const user = document.getElementById("newUsername").value;
-      const pass = document.getElementById("newPassword").value;
+      const contact = document.getElementById("contact").value.trim();
+      const user = document.getElementById("newUsername").value.trim();
+      const pass = document.getElementById("newPassword").value.trim();
 
-      if(user && pass){
-        localStorage.setItem("loginUser", user);
-        localStorage.setItem("loginPass", pass);
+      if(contact && user && pass){
+        // オブジェクトにまとめて保存
+        const newUser = {
+          contact: contact,
+          username: user,
+          password: pass
+        };
+
+        // localStorageにJSON文字列で保存
+        localStorage.setItem("loginUserData", JSON.stringify(newUser));
+
         alert("登録完了！ログインしてください");
-        location.href = "../Login/";
+        location.href = "../Login/"; // Loginページへのリンク
       } else {
         alert("全て入力してください");
       }
