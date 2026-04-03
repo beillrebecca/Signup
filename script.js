f397e03f13b53d12b0df1a9039b59712330cade6
beillrebecca <noanh0810@gmail.com>
2026-04-03 10:01:18 +0900

    script.js

--- script.js
+++ script.js
@@ -4,28 +4,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   const signupBtn = document.getElementById("signupBtn");
 
-  if(signupBtn){
-    signupBtn.addEventListener("click", () => {
-      const contact = document.getElementById("contact").value.trim();
-      const user = document.getElementById("newUsername").value.trim();
-      const pass = document.getElementById("newPassword").value.trim();
+  signupBtn?.addEventListener("click", () => {
+    const emailOrPhone = document.getElementById("newEmailOrPhone")?.value || "";
+    const username = document.getElementById("newUsername").value;
+    const password = document.getElementById("newPassword").value;
 
-      if(contact && user && pass){
-        // オブジェクトにまとめて保存
-        const newUser = {
-          contact: contact,
-          username: user,
-          password: pass
-        };
+    if(emailOrPhone && username && password){
+      // 1人分の情報をオブジェクトにまとめる
+      const userData = {
+        contact: emailOrPhone,
+        username: username,
+        password: password
+      };
 
-        // localStorageにJSON文字列で保存
-        localStorage.setItem("loginUserData", JSON.stringify(newUser));
+      // localStorage に JSON 文字列で保存
+      localStorage.setItem("loginUserData", JSON.stringify(userData));
 
-        alert("登録完了！ログインしてください");
-        location.href = "../Login/"; // Loginページへのリンク
-      } else {
-        alert("全て入力してください");
-      }
-    });
-  }
+      alert("登録完了！ログインしてください");
+      location.href = "../Login/"; // Login ページの index.html に移動
+    } else {
+      alert("全て入力してください");
+    }
+  });
 });
